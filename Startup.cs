09954ae31ae0482cee6b44fd3232006c844dd348
Matthew King <mdking18@gmail.com>
2018-12-02 12:21:07 -0500

    Add Cors (cross origin) service. Needed to allow Angular SPA to access WebAPI proj.

--- Startup.cs
+++ Startup.cs
@@ -22,6 +22,7 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +38,7 @@ namespace DatingApp.API
             }
 
             //app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
