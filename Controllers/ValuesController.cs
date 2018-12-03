46a26988611c8114f609b7f741046b9296fe8d4a
Matthew King <mdking18@gmail.com>
2018-12-02 19:29:20 -0500

    End of Section 3

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -18,7 +20,7 @@ namespace DatingApp.API.Controllers
         {
             _context = context;
         } 
-        // GET api/values
+                
         [HttpGet]
         public async Task<IActionResult> Get()
         {
@@ -26,7 +28,7 @@ namespace DatingApp.API.Controllers
             return Ok(values);
         }
 
-        // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
@@ -51,5 +53,6 @@ namespace DatingApp.API.Controllers
         public void Delete(int id)
         {
         }
+
     }
 }
---
+++ Dtos/UserForRegisterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(8, ErrorMessage = "Password must be between 4 and 8 characters", MinimumLength = 4)]
+        public string Password { get; set; }
+    }
+}
